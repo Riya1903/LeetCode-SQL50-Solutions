3642fc7d2426c58e28c2427004748a8cbf08f427
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-11-13 23:46:58 +0530

    Time: 304 ms (85.68%), Space: 0B (100.00%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,11 @@
+# Wr(ite your MySQL query statement below
+select id ,count(*) as num
+from(
+select requester_id as id
+from requestaccepted
+union all
+select accepter_id
+from requestaccepted) as friend_count
+group by id
+order by num desc
+limit 1
